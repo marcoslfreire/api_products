c9868ac9da62e9d80dbb5eaf755cd9dd1a78fa3f
marcos luciano <marcos3dt@gmail.com>
2023-07-09 00:50:56 -0300

    Criação do CRUD/ API REST FULL

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,81 @@
+package com.example.springboot.controllers;
+
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository  productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        List<ProductModel> productsList = productRepository.findAll();
+        if (!productsList.isEmpty()){
+            for (ProductModel product : productsList){
+                UUID id =  product.getIdProduct();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id")UUID id){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não cadastrado na base de dados!");
+        }
+        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Lista de Produtos!"));
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> UpdateProduct(@PathVariable(value = "id")UUID id,
+                                                @RequestBody @Valid ProductRecordDto productRecordDto){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não cadastrado na base de dados!");
+        }
+        var produtoModel = productO.get();
+        BeanUtils.copyProperties(productRecordDto, produtoModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(produtoModel));
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id")UUID id){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não cadastrado na base de dados!");
+        }
+        productRepository.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Produto deletado com sucesso!");
+    }
+
+
+}
--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package com.example.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.springboot.models.ProductModel;
+import org.springframework.stereotype.Repository;
 
 /**
  * Interface de repositório para gerenciar entidades de Produtos.
@@ -9,5 +10,6 @@ import com.example.springboot.models.ProductModel;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
 }
